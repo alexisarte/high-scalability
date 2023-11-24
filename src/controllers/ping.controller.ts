c266297d63aa8899ee905f72a4b6c5910bac7615
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-11-24 12:20:44 -0300

    Launch Web App container

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -52,4 +52,46 @@ export class PingController {
       headers: Object.assign({}, this.req.headers),
     };
   }
+
+  @get('/sleep', {
+    parameters: [{name: 'ms', schema: {type: 'number'}, in: 'query'}],
+    responses: {
+      '200': {
+        description: 'Sleep in ms',
+        content: {
+          'application/json': {
+            schema: {type: 'string'},
+          },
+        },
+      },
+    },
+  })
+  async sleep(ms: number) {
+    await this.delay(ms);
+    return `Waking up after ${ms} ms`;
+  }
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  @get('/fib', {
+    parameters: [{name: 'num', schema: {type: 'number'}, in: 'query'}],
+    responses: {
+      '200': {
+        description: 'Compute fibonacci',
+        content: {
+          'application/json': {
+            schema: {type: 'number'},
+          },
+        },
+      },
+    },
+  })
+  async fib(num: number) {
+    return this.fibonacci(num);
+  }
+  fibonacci(num: number): number {
+    if (num <= 1) return 1;
+    return this.fibonacci(num - 1) + this.fibonacci(num - 2);
+  }
 }
